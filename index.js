a32a354fc6ed92b70faa77a869e32728c28ee829
DanielGarciaG <daniel.garcia06g1991@gmail.com>
2023-10-16 12:05:13 -0500

    Cambios agregados

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const app = express()
 //mongoose.connect('mongodb://dani:password@mongocom:27017/miapp?authSource=admin')
 
 app.get('/', async (_req, res) => {
-  console.log('listando... animales ingresadossss')
+  console.log('listando... animales agregados')
   //const animales = await Animal.find();
   return res.send("animales")
 })
